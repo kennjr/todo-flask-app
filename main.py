58872429f5453a184bf1e69c78579af8b560cbf3
kennjr <ken.juniour@student.moringaschool.com>
2022-07-26 21:54:44 +0300

    created a list of dummy data objs and passed them to the render_template as an arg

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# the __name__ creates a flask app that's named after the file it's in i.e main
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = [{'description': 'Todo one'}, {'description': 'Todo two'}, {'description': 'Todo three'}]
+    return render_template('index.html', data=data)
