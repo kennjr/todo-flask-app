572333bc09be752d230b36fea4929b7e75218097
kennjr <ken.juniour@student.moringaschool.com>
2022-07-26 23:49:21 +0300

    created the 'create' route for adding a todo to the db and deleted the old data using a for in the index controller

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 # the __name__ creates a flask app that's named after the file it's in i.e main
+from werkzeug.utils import redirect
+
 app = Flask(__name__)
 # the line below configures the flask app to the specified db
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://demo_user:password@localhost:5432/todo_db"
@@ -26,8 +28,24 @@ def index():
     #
     # db.session.add_all([first_todo, second_todo, third_todo])
     # db.session.commit()
+
     data = Todos.query.all()
+    # for item in data:
+    #     db.session.delete(item)
+    # db.session.commit()
     return render_template('index.html', data=data)
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # get the data from the form
+    description = request.form.get('description')
+    # add the data to the server
+    todo = Todos(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+    # return render_template("index.html")
 
 
 if __name__ == '__main__':
