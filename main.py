0ef79c1b26870ecdfda998fe23413d412e2cc8bb
kennjr <ken.juniour@student.moringaschool.com>
2022-07-26 22:10:50 +0300

    configured the app with the db, created the todos model class and connected the app+db to the migrations

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # the __name__ creates a flask app that's named after the file it's in i.e main
 app = Flask(__name__)
+# the line below configures the flask app to the specified db
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://demo_user:password@localhost:5432/todo_db"
+# the line below links SQLAlchemy to the flask app
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Todos(db.Model):
+    __tablename__ = 'todos'
+    pk = db.Column(db.Integer, primary_key=True, nullable=False)
+    description = db.Column(db.String(), nullable=False)
+
 
 @app.route('/')
 def index():
-    data = [{'description': 'Todo one'}, {'description': 'Todo two'}, {'description': 'Todo three'}]
+
+    data = Todos.query.all()
     return render_template('index.html', data=data)
